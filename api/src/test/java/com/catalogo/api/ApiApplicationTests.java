7dc2764c096351c21418ec02c1818c7ccaf249c0
Lewy Alves <lewyalves78@gmail.com>
2023-11-15 21:14:28 -0300

    api do projeto

---
+++ api/src/test/java/com/catalogo/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalogo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
